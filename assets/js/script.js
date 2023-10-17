4d2d00e2f17aa8b1bbd81e84626a7515d89b3c40
Ghitado <thiaguinho.020405@gmail.com>
2023-10-17 16:22:56 -0300

    termino media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 window.addEventListener("scroll", function() {
-  const nav = document.querySelector('.header')
-  nav.classList.toggle("sticky", this.window.scrollY > 0)
+  const navMobile = document.querySelector('.navbar-mobile')
+  navMobile.classList.toggle("position", this.window.scrollY > 50)
 })
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
