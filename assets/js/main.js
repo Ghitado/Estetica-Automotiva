650c3070623d3182064b9fae519ce734e6b24824
Ghitado <thiaguinho.020405@gmail.com>
2023-10-20 11:05:10 -0300

    up na estrutura de pastas e componentizacao do header, contato e footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+$("#header").load('../../components/header.html');
+$("#contact").load('../../components/contact.html');
+$("#footer").load('../../components/footer.html');
+
+
 window.addEventListener("scroll", function() {
   const navMobile = document.querySelector('.navbar-mobile')
   navMobile.classList.toggle("position", this.window.scrollY > 50)
